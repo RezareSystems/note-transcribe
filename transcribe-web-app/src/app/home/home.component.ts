037c08b81846f965e026aa18020620ec337d3332
Gerard De Leon <gerard.de.leon@rezare.co.nz>
2019-08-15 12:02:15 +1200

    add record and upload functionality

--- transcribe-web-app/src/app/home/home.component.ts
+++ transcribe-web-app/src/app/home/home.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { RecordingService } from '../recording.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   columns = [
     'ref',
@@ -18,9 +20,43 @@ export class HomeComponent implements OnInit {
     'transcribedfile'
   ];
 
-  constructor() { }
+  isRecording;
+  blob;
+  blobUrl;
+
+  subs: Subscription[] = [];
+
+  constructor(private recordingService: RecordingService) { }
 
   ngOnInit() {
+    this.subs.push(this.recordingService.isRecording.subscribe(val => this.isRecording = val));
+    this.subs.push(this.recordingService.blob.subscribe(val => this.blob = val));
+    this.subs.push(this.recordingService.blobUrl.subscribe(val => this.blobUrl = val));
+  }
+
+  ngOnDestroy() {
+    this.subs.forEach(s => s.unsubscribe());
+  }
+
+  recordButton() {
+    if (this.isRecording) {
+      this.recordingService.stopRecording();
+    } else {
+      this.recordingService.startRecording();
+    }
+  }
+
+  clear() {
+    this.recordingService.clearRecording();
+  }
+
+  onFileChanged(event) {
+    const fileList: FileList = event.target.files;
+    const file = fileList[0];
+    if (file) {
+      this.recordingService.setBlobManual(file);
+    }
+    event.target.value = '';
   }
 
 }
---
+++ transcribe-web-app/src/app/recording.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import * as MicRecorder from 'mic-recorder-to-mp3';
+import { BehaviorSubject } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecordingService {
+
+  private isRecordingSubject = new BehaviorSubject<boolean>(false);
+  private blobUrlSubject = new BehaviorSubject<any>(null);
+  private blobSubject = new BehaviorSubject<any>(null);
+  private recorder;
+
+  get isRecording() {
+    return this.isRecordingSubject.asObservable();
+  }
+
+  get blobUrl() {
+    return this.blobUrlSubject.asObservable();
+  }
+
+  get blob() {
+    return this.blobSubject.asObservable();
+  }
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.recorder = new MicRecorder({
+      bitRate: 128
+    });
+  }
+
+  startRecording() {
+    this.recorder.start().then(() => {
+      this.isRecordingSubject.next(true);
+    }).catch(err => console.error(err));
+  }
+
+  stopRecording() {
+    this.recorder.stop().getMp3().then(([buffer, blob]) => {
+      const file = new File(buffer, 'test.mp3', {
+        type: blob.type,
+        lastModified: Date.now()
+      });
+      this.blobSubject.next(blob);
+      this.blobUrlSubject.next(this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(file)));
+      this.isRecordingSubject.next(false);
+    }).catch(err => console.error(err));
+  }
+
+  clearRecording() {
+    this.blobSubject.next(null);
+    this.blobUrlSubject.next(null);
+  }
+
+  setBlobManual(file) {
+    this.blobSubject.next(file);
+    this.blobUrlSubject.next(this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(file)));
+  }
+}
